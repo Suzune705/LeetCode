11be606ac92c922f135b848173fb448dd9b36f45
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-10-05 02:33:25 +0700

    feat(array): solved

--- Prolems/problem_3065.java
+++ Prolems/problem_3065.java
@@ -2,16 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class problem_3065 {
-	public static  int minOperations(int[] nums, int k) {
-		int operation = 0 ;
-		for (int x : nums) {
-			if (x < k) {
-				operation++ ;
-			}
-		}
-		return operation ;
-	}
-	public static void main(String[] args) {
+    public static  int minOperations(int[] nums, int k) {
+        int operation = 0 ;
+        for (int x : nums) {
+            if (x < k) {
+                operation++ ;
+            }
+        }
+        return operation ;
+    }
+    public static void main(String[] args) {
 
-	}
+    }
 }
