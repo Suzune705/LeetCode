1ca6cd3bd589a32bd5052f24b6052dca857c8dc4
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-10-09 23:44:34 +0700

    feat(array): solved

---
+++ Prolems/problem_1313.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class problem_1313 {
+
+
+	public static int[] decompressRLElist(int[] nums) {
+
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0 ; i <= nums.length - 1 ; i += 2) {
+			int fre = nums[i];
+			int val = nums[i + 1];
+			int[] pair = new int[fre];
+			int j = 0 ;
+			while ( j < pair.length ) {
+				list.add(val);
+				++j ;
+			}
+		}
+		int[] tmp = list.stream().mapToInt(Integer::intValue).toArray();
+		return tmp  ;
+	}
+
+	public static void main(String[] args) {
+		int[] a = {1, 2, 3, 4};
+		// decompressRLElist(a);
+		for (int x : decompressRLElist(a)) {
+			System.out.print(x);
+		}
+	}
+}
+
