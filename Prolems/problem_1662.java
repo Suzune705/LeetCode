118729b21db071a4f30b235de113a0d6b90f7033
Suzune <124695777+Suzune705@users.noreply.github.com>
2025-11-15 00:14:46 +0700

    solve(string): problem_1662

---
+++ Prolems/problem_1662.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class problem_1662 {
+
+	public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+		String tmp1 = "";
+		String tmp2  = "";
+		for (String s : word1) {
+			tmp1 += s ;
+		}
+		for (String s : word2) {
+			tmp2 += s ;
+		}
+		return tmp1.equals(tmp2);
+	}
+
+	public static void main(String[] args) {
+		String[] a = {
+			"ab",
+			"c"
+		};
+		String[] b = {
+			"a",
+			"bc"
+		};
+		if (arrayStringsAreEqual(a, b)) {
+			System.out.print("true");
+		} else {
+			System.out.print("false");
+		}
+	}
+}
